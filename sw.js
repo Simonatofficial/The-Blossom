7934801a3481b2b833f82fe8b1855d23a04d6aa6
Cosmo <simonatofficial@gmail.com>
2026-06-10 19:15:27 -0600

    chore(release): bump SW cache to blossom-v2 for the CR release

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 
 importScripts('./sw-assets.js'); // defines self.SW_ASSETS (generated list)
 
-const CACHE = 'blossom-v1';
+const CACHE = 'blossom-v2';
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(self.SW_ASSETS)));
